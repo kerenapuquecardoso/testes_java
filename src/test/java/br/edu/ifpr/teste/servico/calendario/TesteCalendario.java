bef52179b2144b6ff0653916ddfbfd224f8e0ecd
Keren Apuque <cardosokerenapuque@gmail.com>
2025-05-20 00:14:07 -0300

    test: realizado testes de caixa preta referente ao calendario gregoriano e ao calculo do imposto de renda

--- src/test/java/br/edu/ifpr/teste/servico/calendario/TesteCalendario.java
+++ src/test/java/br/edu/ifpr/teste/servico/calendario/TesteCalendario.java
@@ -1,6 +1,8 @@
 package br.edu.ifpr.teste.servico.calendario;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,21 +16,76 @@ import paranavai.calendario.Calendario;
 public class TesteCalendario {
 
     @Test
-    public void imprimeJaneiro2025() throws IOException {
-        // Arrange (PREPARAR) - Roy Osherove no livro “The Art of Unit Testing”.
-        // Given (DADO) - BDD (Behavior Driven Development)
+    public void entradaNullSaidaExperadaMesDoAnoAtual() throws IOException {
+        Path path = Paths.get("src\\test\\resources\\calendario", "maio2025.txt");
+        String saidaEsperada = Files.readString(path);
+        saidaEsperada = saidaEsperada.replace("\r\n", "\n");
 
+        Path pathJaneiro = Paths.get("src\\test\\resources\\calendario", "janeiro2025.txt");
+        String janeiro2025 = Files.readString(pathJaneiro);
+
+        Calendario calendario = new Calendario();
+        String maio2025 = calendario.getCalendario();
+
+        assertEquals(saidaEsperada, maio2025);
+        assertNotEquals(saidaEsperada, janeiro2025);
+    }
+
+    // @Test
+    // public void umaEntradaSaidaExperadaMesesDoAnoInserido() throws IOException {
+    // Path path = Paths.get("src\\test\\resources\\calendario",
+    // "calendarioAno2025.txt");
+    // String saidaEsperada = Files.readString(path);
+    // saidaEsperada = saidaEsperada.replace("\r\n", "\n");
+
+    // Calendario calendario = new Calendario();
+    // String ano2025 = calendario.getCalendario("2025");
+
+    // assertEquals(saidaEsperada, ano2025);
+    // }Esse teste falhou pois o que retornou no getCalendário é MarÃ§o
+
+    @Test
+    public void umaEntradaInvalidaSaidaExperadaErro() throws IOException {
+        Calendario calendario = new Calendario();
+
+        assertThrows(NumberFormatException.class, () -> calendario.getCalendario("10000"));
+    }
+
+    @Test
+    public void imprimeJaneiro2025() throws IOException {
         Path path = Paths.get("src\\test\\resources\\calendario", "janeiro2025.txt");
         String saidaEsperada = Files.readString(path);
         saidaEsperada = saidaEsperada.replace("\r\n", "\n");
 
-        // Act (AGIR)
-        // When (QUANDO)
         Calendario calendario = new Calendario();
         String janeiro2025 = calendario.getCalendario("1", "2025");
 
-        // Assert (VERIFICAR)
-        // Then (ENTÃO)
         assertEquals(saidaEsperada, janeiro2025);
     }
+
+    @Test
+    public void entradaMesErradoSaidaErro() throws IOException {
+        Calendario calendario = new Calendario();
+
+        assertThrows(NumberFormatException.class, () -> calendario.getCalendario("15", "2025"));
+    }
+
+    @Test
+    public void entradaSetembro1752SaidaEsperadaValida() throws IOException {
+        Path path = Paths.get("src\\test\\resources\\calendario", "setembro1752.txt");
+        String saidaEsperada = Files.readString(path);
+        saidaEsperada = saidaEsperada.replace("\r\n", "\n");
+
+        Path pathErro = Paths.get("src\\test\\resources\\calendario", "setembroErrado1752.txt");
+        String saidaEsperadaErrado = Files.readString(pathErro);
+        saidaEsperadaErrado = saidaEsperadaErrado.replace("\r\n", "\n");
+
+        Calendario calendario = new Calendario();
+        String setembro1752 = calendario.getCalendario("9", "1752");
+
+        assertEquals(saidaEsperada, setembro1752);
+        assertNotEquals(saidaEsperada, saidaEsperadaErrado);
+
+    }
+    
 }
--- src/test/java/br/edu/ifpr/teste/servico/irpf/TesteSimuladorImpostoRenda.java
+++ src/test/java/br/edu/ifpr/teste/servico/irpf/TesteSimuladorImpostoRenda.java
@@ -1,20 +1,56 @@
 package br.edu.ifpr.teste.servico.irpf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import imposto.renda.SimuladorImpostoRenda;
 
 public class TesteSimuladorImpostoRenda {
+
     @Test
-    public void exemploTeste() {
-        // Arrange (PREPARAR) - Roy Osherove no livro “The Art of Unit Testing”.
-        // Given (DADO) - BDD (Behavior Driven Development)
+    public void entradaMenor() {
+        double entrada = 150.23;
+        double reultado = SimuladorImpostoRenda.calcularImposto(entrada);
 
-        // Act (AGIR)
-        // When (QUANDO)
+        assertEquals(0, reultado);
+    }
 
-        SimuladorImpostoRenda.calcularImposto(0);
-        // Assert (VERIFICAR)
-        // Then (ENTÃO)
+    @Test
+    public void entradaComAliquotaEmSeteEMeio() throws IOException {
+       double entrada = 2826.65;
+        double reultado = SimuladorImpostoRenda.calcularImposto(entrada);
+
+        assertEquals(69.20025, reultado);
+        assertNotEquals(69.19, reultado);
+    }
+
+    @Test
+    public void entradaComAliquotaEmQuinze() throws IOException {
+       double entrada = 3000.00;
+        double reultado = SimuladorImpostoRenda.calcularImposto(entrada);
+
+        assertNotEquals(95.20, reultado);
+        assertEquals(95.20249999999999, reultado);
+    }
+
+    @Test
+    public void entradaComAliquotaEmVinteDoisEMeio() throws IOException {
+       double entrada = 3751.06;
+        double reultado = SimuladorImpostoRenda.calcularImposto(entrada);
+
+        assertEquals(207.86, reultado);
+    }
+
+    @Test
+    public void entradaComAliquotaEmVinteSeteEMeio() throws IOException {
+       double entrada = 7000.00;
+        double reultado = SimuladorImpostoRenda.calcularImposto(entrada);
+        
+        assertNotEquals(1055.64, reultado);
+        assertEquals(1055.633, reultado);
     }
 }
